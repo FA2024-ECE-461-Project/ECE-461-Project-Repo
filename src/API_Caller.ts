923f78069255940d2da26cfbd7bd04c10087a3d8
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-09-08 12:37:18 -0400

    skeleton implementation of API_Caller and TokenBuffer

--- src/API_Caller.ts
+++ src/API_Caller.ts
@@ -1,10 +1,23 @@
 // this is a module responsible for making API calls to the urls (uris)
-import Queue from 'Queue';
+import {TokenBuffer} from './TokenBuffer';
 
 class API_Caller {
-    private GitHubTokenQueue: Queue;
-    private successfulCall;
+    private GitHubTokens: TokenBuffer;
+    private successfulCall: boolean;
     
+    constructor(tokens: [string, string, string, string]) {
+        this.GitHubTokens = new TokenBuffer(tokens);
+    }
     
+    makeCallTo(url: string, action: string) {
+        // use await/sync function to make request to url
+        // records the call status to successfulCall
+        // stores the json rendered 
+    }
+
+    is_successfulCall(): boolean {
+        //return the status of the most recent API call
+        return this.successfulCall;
+    }
 
 }
--- src/Queue.ts
+++
@@ -1,7 +0,0 @@
-// internal structure referred by API_Caller
-// basic queue that obeys FIFO (First-In-First-Out)
-// this is meant to store GitHub tokens for each team member
-
-interface Queue<T> {
-    
-}
