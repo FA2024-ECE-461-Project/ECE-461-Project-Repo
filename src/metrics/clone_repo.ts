15c6aa84db3f467d20e8d860bd883b91abfd3503
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-09-15 15:56:56 -0400

    add removeRepo function

--- src/metrics/clone_repo.ts
+++ src/metrics/clone_repo.ts
@@ -1,10 +1,18 @@
-// this function clones a GitHub repository to a local directory
+// this file has functions that clone a repository from GitHub and remove cloned repos
 import * as path from 'path';
 import * as fs from 'fs';
 import * as http from 'isomorphic-git/http/node';
 import * as git from 'isomorphic-git';
 import { URL } from 'url';
+import { exec } from 'child_process'; // for cloning remote repos to do anaylsis
 
+/*
+    * Clone a GitHub repository to the local filesystem
+    * @param githubUrl: string - The URL of the GitHub repository to clone
+    * @returns: string The path to the cloned repository
+    * 
+    * This function works on eceprog, but not sure on other maches, may need to fix later
+*/
 async function cloneRepo(githubUrl: string): Promise<string> {
   // Step 1: Validate the GitHub URL
   let repoPath: string;
@@ -35,10 +43,28 @@ async function cloneRepo(githubUrl: string): Promise<string> {
     singleBranch: true,
     depth: 1,
   });
-
   // Step 3: Return the path to the cloned repository
   return repoPath;
 }
+
+/*
+    * Remove a cloned repository from the local filesystem
+    * @param repoPath: string - The path to the cloned repository
+    * @returns: boolean - Indicates whether the repository was successfully removed
+    * if the repository does not exist, throw an error
+*/
+async function removeRepo(repoPath: string): Promise<Boolean> {
+    // Step 1: Validate the repository path
+    if (!fs.existsSync(repoPath)) {
+        throw new Error('Repository does not exist');
+    }
+    
+    // Step 2: Remove the repository
+    await exec(`rm -rf ${repoPath}`);
+    // Step 3: Return true to indicate success
+    return true;
+}
+
 
 // Example usage
 const githubUrl = 'https://github.com/cloudinary/cloudinary_npm';
@@ -48,3 +74,5 @@ cloneRepo(githubUrl).then(repoPath => {
 }).catch(error => {
   console.error('Error:', error.message);
 });
+
+export { cloneRepo , removeRepo };
