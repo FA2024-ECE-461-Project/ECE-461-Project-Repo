494a1f8b3c989771a6347bea5da0f570542971e0
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-09-18 09:45:06 -0400

    trying to mock tests

--- test/correctness_helper.test.ts
+++ test/correctness_helper.test.ts
@@ -3,11 +3,15 @@ import {cloneRepo, removeRepo} from "../src/metrics/clone_repo";
 import exp from "constants";
 
 const testRepoUrl = "https://github.com/cloudinary/cloudinary_npm";
-
 let clonedPath: string; //declare here so all tests have access
+// setup mock isomorphic-git (optional, maybe cloning a small repo and do our tests on it is fine)
+// jest.mock('isomorphic-git');
+// const mockedGit = require('isomorphic-git');
 beforeAll(async () => {
 // setting up: mock isomorphic-git: do this later
     clonedPath = await cloneRepo(testRepoUrl);
+    expect(clonedPath).not.toBeNull();
+    // mockedGit.clone.mockResolvedValueOnce(clonedPath);
 });
 
 describe('Correctness Helpers', () => {
