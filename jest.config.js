c19eb97317d58f2102fd3649b040e6fd0fa11a9d
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-09-18 00:36:32 -0400

    add tests for correctness_helper

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+
 // module.exports = {
 //   testEnvironment: "node",
 //   transform: {
 //     "^.+.tsx?$": ["ts-jest",{}],
 //   },
 // };
+//above syntax is for other versions of jest, for jest 27.0.6, the syntax is as below
 export default {
   testEnvironment: "node",
   transform: {
---
+++ test/correctness_helper.test.ts
@@ -0,0 +1,32 @@
+import { _getCoverageScore } from "../src/metrics/correctness_helpers";
+import {cloneRepo, removeRepo} from "../src/metrics/clone_repo";
+import exp from "constants";
+
+const testRepoUrl = "https://github.com/cloudinary/cloudinary_npm";
+
+let clonedPath: string; //declare here so all tests have access
+beforeAll(async () => {
+// setting up: mock isomorphic-git: do this later
+    clonedPath = await cloneRepo(testRepoUrl);
+});
+
+describe('Correctness Helpers', () => {
+  test('_getCoverageScore with valid cloned repo', async () => {
+    const coverageScore = await _getCoverageScore(clonedPath);
+    expect(coverageScore).toBeGreaterThan(0);
+    expect(coverageScore).toBeLessThanOrEqual(1);
+  });
+  test("getCoverageScore with invalid cloned repo", async () => {
+    const invalidPath = "invalid/path";
+    const coverageScore = await _getCoverageScore(invalidPath);
+    expect(coverageScore).toBe(0);
+  });
+})
+
+afterAll(async () => {
+  // teardown: remove cloned repo
+  // can remove this if mock isomorphic-git is implemented
+  const removed = await removeRepo(testRepoUrl);
+  expect(removed).toBeTruthy();
+});
+
