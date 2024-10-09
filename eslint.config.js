ab26594b48c4b6e5981383b6fbe69656acd0aa92
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-10-09 11:38:51 -0400

    trying TS eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
-import config from "eslint-config-airbnb-base";
+// @ts-check
 
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
-  ...[].concat(config),
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
